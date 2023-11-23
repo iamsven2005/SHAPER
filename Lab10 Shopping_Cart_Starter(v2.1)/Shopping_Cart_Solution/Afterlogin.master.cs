c9cafd3ecfc52f604bd6c1201b47539168300d08
Sven <iamsven2005@gmail.com>
2023-11-23 14:50:28 +0800

    new

--- Lab10 Shopping_Cart_Starter(v2.1)/Shopping_Cart_Solution/Afterlogin.master.cs
+++ Lab10 Shopping_Cart_Starter(v2.1)/Shopping_Cart_Solution/Afterlogin.master.cs
@@ -11,14 +11,25 @@ using Salt_Password_Sample;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 using System.Data;
 using Microsoft.Identity.Client;
+using System.IO;
+using System.Web.Services.Description;
 
 public partial class MasterPage : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+
         lblAftLogin.Text = Session["Email"].ToString();
         Credit.Text = Session["Credit"].ToString();
-        Theme.Text = Session["Theme"].ToString();
+        string filePath = Server.MapPath("~/Users/themes.txt");
+        string[] lines = File.ReadAllLines(filePath);
+        string targetLine = Array.Find(lines, line => line.StartsWith(lblAftLogin.Text + ":"));
+        string[] parts = targetLine.Split(new char[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+        Theme.Text = parts[1].Trim();
+        //Session["Theme"].ToString();
+        //DeleteFromFile("robots.txt", "Line to delete");
+        //AppendToFile("robots.txt", "New line to append");
+
     }
     protected void btnSearch_Click(object sender, EventArgs e)
     {
@@ -34,7 +45,64 @@ public partial class MasterPage : System.Web.UI.MasterPage
     }
     protected void SaveTheme_Click(object sender, EventArgs e)
     {
-        //todo add sql
-    }
+        string filePath = Server.MapPath("~/Users/themes.txt");
+        string targetEmail = lblAftLogin.Text;
+        string newTheme = Theme2.Text;
+        string[] lines = File.ReadAllLines(filePath);
+        int index = Array.FindIndex(lines, line => line.StartsWith(targetEmail + ":"));
+        string[] parts = lines[index].Split(new char[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+        if (parts.Length == 2)
+        {
+            lines[index] = $"{targetEmail}: {newTheme}";
+            File.WriteAllLines(filePath, lines);
+            Response.Redirect(Request.RawUrl, false);
+            Context.ApplicationInstance.CompleteRequest();
+        }
 
- }
+
+
+    }
+    protected string[] GetTextFileLines()
+    {
+        string filePath = Server.MapPath("~/Users/themes.txt"); // Adjust the path based on your file location
+        return File.Exists(filePath) ? File.ReadAllLines(filePath) : new string[0];
+    }
+    //protected void DeleteFromFile(string filePath, string lineToDelete)
+    //{
+    //    // Combine the file path with the application's root
+    //    filePath = Server.MapPath("~/" + filePath);
+
+    //    try
+    //    {
+    //        // Read all lines from the file
+    //        string[] lines = File.ReadAllLines(filePath);
+
+    //        // Find and remove the line to delete
+    //        lines = Array.FindAll(lines, line => !line.Contains(lineToDelete));
+
+    //        // Write the modified lines back to the file
+    //        File.WriteAllLines(filePath, lines);
+    //    }
+    //    catch (Exception ex)
+    //    {
+    //        // Handle exceptions, e.g., log or display an error message
+    //        Response.Write($"Error deleting from the file: {ex.Message}");
+    //    }
+    //}
+    //protected void AppendToFile(string filePath, string lineToAppend)
+    //{
+    //    // Combine the file path with the application's root
+    //    filePath = Server.MapPath("~/" + filePath);
+
+    //    try
+    //    {
+    //        // Append the line to the text file
+    //        File.AppendAllText(filePath, lineToAppend + Environment.NewLine);
+    //    }
+    //    catch (Exception ex)
+    //    {
+    //        // Handle exceptions, e.g., log or display an error message
+    //        Response.Write($"Error appending to the file: {ex.Message}");
+    //    }
+    //}
+}
